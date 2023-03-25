c0ec9ff1ae3a0691942a28570d7aec09b9820063
ridmajayasundara <e18155@eng.pdn.ac.lk>
2023-03-25 08:09:42 +0530

    authentication to spotify API added

--- get-spotify-list.py
+++ get-spotify-list.py
@@ -3,6 +3,12 @@ import requests
 import os
 from dotenv import load_dotenv
 
+import csv
+import re
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
 load_dotenv() 
 ################################################################################
 
@@ -14,4 +20,12 @@ OUTPUT_FILE_NAME = "track_info.csv"
 PLAYLIST_LINK = 'https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M'
 ################################################################################
 
+
+# authenticate
+client_credentials_manager = SpotifyClientCredentials(
+                                            client_id=CLIENT_ID, 
+                                            client_secret=CLIENT_SECRET)
+
+# create spotify session object
+session = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
