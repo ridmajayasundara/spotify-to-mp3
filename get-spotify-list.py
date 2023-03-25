d4577bfe4b7c41046260e38bb21743e402b23b1b
ridmajayasundara <e18155@eng.pdn.ac.lk>
2023-03-25 08:29:11 +0530

    save list in a .csv file

--- get-spotify-list.py
+++ get-spotify-list.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 CLIENT_ID = os.getenv("CLIENT_ID", "")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET", "")
-OUTPUT_FILE_NAME = "track_info.csv"
+OUTPUT_FILE_NAME = "spotify_playlist_song-and-artist.csv"
 
 # change for your target playlist
 PLAYLIST_LINK = 'https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M?si=b17f987ba59f490a'
@@ -35,6 +35,25 @@ if match := re.match(r"https://open.spotify.com/playlist/(.*)\?", PLAYLIST_LINK)
 else:
     raise ValueError("Please add complete link to PLAYLIST_LINK (copy from app, not the web): Expected format: https://open.spotify.com/playlist/...")
 
+
+
+# get list of tracks in a given playlist (note: max playlist length 100)
+tracks = session.playlist_tracks(playlist_uri)["items"]
+
+
+# create csv file
+with open(OUTPUT_FILE_NAME, "w", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    
+    # extract name and artist
+    for track in tracks:
+        name = track["track"]["name"]
+        artists = ", ".join(
+            [artist["name"] for artist in track["track"]["artists"]]
+        )
+
+        # write to csv
+        writer.writerow([name, artists])
 
 
 
