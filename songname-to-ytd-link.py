cea91e1d9e82ac5cb8fea434bb5207b2702fd268
ridmajayasundara <e18155@eng.pdn.ac.lk>
2023-03-29 22:41:31 +0530

    song names to youtube links conversion process done

--- songname-to-ytd-link.py
+++ songname-to-ytd-link.py
@@ -1,12 +1,36 @@
 from youtubesearchpython import VideosSearch
 import json 
+from time import sleep
 
-videosSearch = VideosSearch('Flowers,Miley Cyrus', limit = 1)
-data = videosSearch.result()
-details = (json.dumps(data, indent=4, sort_keys=True))
-json_obj = json.loads(details)
-link = json_obj['result'][0]['channel']['link']
-print(link)
+try:
+    with open('spotify_playlist_song-and-artist.csv', 'r') as readFile:
+        for line in readFile:
+            songTitle = line.strip()
+            try:
+                videosSearch = VideosSearch(songTitle, limit = 1)
+                data = videosSearch.result()
+            except:
+                print('error trying to search youtube')
+                sleep(1)
+                continue
+
+
+            details = (json.dumps(data, indent=4, sort_keys=True)) #can be used to print all the details coming 
+
+            json_obj = json.loads(details)
+            link = json_obj['result'][0]['channel']['link']
+            try:
+                with open('song_ytd_links.txt', 'a') as writeFile:
+                    writeFile.write(link+'\n')
+            except:
+                print('error tring to write to the file')
+                continue
+except:
+    print('error with the songname-to-ytd file')
+
+
+
+
 
 
 
